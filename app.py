90d50c7486a07f65663cd3684f002b4468cfe9f7
a368148-alt <a368148@uach.mx>
2025-10-29 08:07:35 -0700

    Fix typo in session_state variable name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Ejemplo para usar sesión_ state")
 
 if "count" not in st.sesión_state:
-  st.sesion_state["count"] = 0
+  st.sesión_state["count"] = 0
 
 st.write(st.sesión_state)
 
