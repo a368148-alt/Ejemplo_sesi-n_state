58821c1a7a380c917c083465ad8834c1b030b85d
a368148-alt <a368148@uach.mx>
2025-10-29 08:04:32 -0700

    Correct session_state spelling in app.py
    Fix typo in session_state reference.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Ejemplo para usar sesión_ state")
 
-if "count" not in st.sesion_state:
+if "count" not in st.sesión_state:
   st.sesion_state["count"] = 0
   
 count = 0
